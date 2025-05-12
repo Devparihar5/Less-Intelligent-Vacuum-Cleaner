3cacb76beaa32b03ac50f984697ab64a8032470e
devpariahr5 <devendra.parihar@accomplishr.com>
2025-05-12 22:26:25 +0530

    added allow_unsafe_werkzeug as true

--- app.py
+++ app.py
@@ -865,4 +865,4 @@ if __name__ == '__main__':
 
     print(f"Starting {app.config['TITLE']} Simulation Web Interface...")
     print("Open your browser and navigate to: http://localhost:5000")
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
